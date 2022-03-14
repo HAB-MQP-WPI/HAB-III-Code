ed5340848ea2455ad3469c14519011025a3d49e5
madelineKasznay <101574146+madelineKasznay@users.noreply.github.com>
2022-03-14 10:22:44 -0400

    Create MQP_UV_test.ino

---
+++ PayloadCode/ArduinoExamples/MQP_UV_test.ino
@@ -0,0 +1,20 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+  float sensorVoltage; 
+  float sensorValue;
+ 
+  sensorValue = analogRead(A3);
+  sensorVoltage = sensorValue/1024*5;
+  Serial.print("sensor reading = ");
+  Serial.print(sensorValue);
+  Serial.println("");
+  Serial.print("sensor voltage = ");
+  Serial.print(sensorVoltage);
+  Serial.println(" V");
+  delay(1000);
+}
